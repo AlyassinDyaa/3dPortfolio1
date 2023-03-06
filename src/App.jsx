a0dad29b18d84dd160babe9d15ad23d5fa0e8c41
Dyaa Alyassin <dyaaalyassin@yahoo.com>
2023-03-05 22:39:45 -0500

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { BrowserRouter } from "react-router-dom"
+import { About, Contact , Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "../src/components/index"
 
 const App = () => {
   return (
-    <div>
-      APP
-    </div>
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar/>
+          <Hero/>
+        </div>
+        <About/>
+        <Experience/>
+        <Tech/>
+        <Works/>
+        <Feedbacks/>
+
+        <div className='relative z-0' >
+          <Contact/>
+          <StarsCanvas/>
+        </div>
+
+      </div>
+    </BrowserRouter>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React , { useEffect, useState } from 'react'
+import { Link } from "react-router-dom"
+import { styles } from "../styles"
+import { navLinks } from "../Constants/index"
+import { logo, menu, close } from "../assets/index"
+
+import LOGO from "../assets/logo.png"
 
 const Navbar = () => {
+
+  const [active, setActive] = useState("")
+
   return (
-    <div>Navbar</div>
+    <nav
+      /* w-full: this class sets the width of the element to 100% of its container.
+        flex: this class applies the Flexbox layout to the element, allowing its child elements to be arranged in a flexible and responsive way.
+        items-center: this class centers the child elements vertically within the Flexbox container.
+        py-5: this class applies padding of 5 pixels on the y-axis (top and bottom) to the element.
+        fixed: this class fixes the position of the element to the viewport, so it remains in the same place even when the user scrolls the page.
+        top-0: this class positions the element at the top of its container.
+        z-20: this class sets the z-index of the element to 20, which determines its position in the vertical stacking order of elements on the page.
+        bg-primary: this class sets the background color of the element to a color called "primary".
+      */
+      className= {  `${styles.paddingX}  w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+          <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+              <Link 
+                to = "/"
+                className = 'flex items-center gap-2'
+                onClick = {() => {
+                  setActive("");
+                  window.scrollTo(0,0);
+                }}
+              >
+                <img src = {LOGO} className = 'w-9 h-9 object-contain' alt='logo' />
+                <p className = 'text-white text-[18px] font-bold cursor-pointer' >D'Yaa <span className = 'sm:block hidden'>Alyassin</span></p>
+              </Link>
+          </div>
+    </nav>
   )
 }
 
